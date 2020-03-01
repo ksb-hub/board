89d5abdb7a2930e55856d43b29a7978c2a4923f1
ksb-hub <61408221+ksb-hub@users.noreply.github.com>
2020-03-01 15:16:58 +0900

    Delete data.py

--- data.py
+++
@@ -1,8 +0,0 @@
-dirName = 'data'
-
-try:
-    # Create target Directory
-    os.mkdir(dirName)
-    print("Directory " , dirName ,  " Created ") 
-except FileExistsError:
-    print("Directory " , dirName ,  " already exists")
